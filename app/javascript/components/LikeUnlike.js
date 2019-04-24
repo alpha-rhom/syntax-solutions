9280fd0867948f6dda2a7f44ab1f788d42aa15ac
Luke Schoenberger <lukeschoenberger@MatthewohensMBP.localdomain>
2019-04-24 12:01:10 -0700

    fixed like and comment

--- app/javascript/components/LikeUnlike.js
+++ app/javascript/components/LikeUnlike.js
@@ -11,26 +11,23 @@ class LikeUnlike extends React.Component {
         markdown: this.props.librarys.markdown,
         likes: this.props.librarys.likes
       },
-      likeTotal: []
+      likeTotal: 0
     }
   }
 
 
-  componentDidMount(){
-    this.setState((prevState) => ({ likeTotal: prevState.likeTotal }))
-  }
 
   handleLikeUnlike = (newLike, e) => {
     let title = this.props.librarys.title
     let desc = this.props.librarys.desc
     let markdown = this.props.librarys.markdown
+    let comments = this.props.librarys.comments
     let likes = newLike
     let id = this.props.libraryId
-    let library = {id: id, title: title, desc: desc,  markdown:  markdown, likes: likes}
+    let library = {id: id, title: title, desc: desc,  markdown:  markdown, likes: likes, comments: comments}
     this.props.handleUpdate(library)
-    
-    // animation
 
+    // animation
     if (e.target.className.includes('thumbs')) {
       let savedEvent = e.target
 
@@ -44,28 +41,33 @@ class LikeUnlike extends React.Component {
 
 
   handleLike = (e) => {
-      this.setState({likeTotal: this.state.likeTotal + 1})
+
+    this.state.likeTotal < 1 ? this.setState({likeTotal: this.state.likeTotal + 1}) : ''
       if (this.state.likeTotal < 1) {
-      let currentLike = typeof this.props.librarys.likes === 'number' ? this.props.librarys.likes : 0
-      let newLike = currentLike + 1
-      this.handleLikeUnlike(newLike, e)
-      } else {
-        console.log('hello')
-      }
+        let currentLike = typeof this.props.librarys.likes === 'number' ? parseInt(this.props.librarys.likes) : 0
+        let newLike = currentLike + 1
+        this.handleLikeUnlike(newLike, e)
+      } 
+
   }
 
   handleDislike = (e) => {
-    let currentLike = typeof this.props.librarys.likes === 'number' ? this.props.librarys.likes : 0
-    let newLike = currentLike - 1
-    this.handleLikeUnlike(newLike, e)
+    this.state.likeTotal > -1 ? this.setState({likeTotal: this.state.likeTotal - 1}) : ''
+
+    if (this.state.likeTotal > - 1) {
+      let currentLike = typeof this.props.librarys.likes === 'number' ? parseInt(this.props.librarys.likes) : 0
+      let newLike = currentLike - 1
+      this.handleLikeUnlike(newLike, e)
+    } 
   }
 
   render () {
     return (
       <React.Fragment>
-        <div className="likes">
-          <Icon className='thumbsup' name='thumbs up outline' onClick={this.handleLike}/><span>{this.state.form.likes}</span><Icon className='thumbsdown' name='thumbs down outline' onClick={this.handleDislike} />
-        </div>
+
+        <Icon className='thumbsup' name='thumbs up outline' onClick={this.handleLike}/>
+        <Icon className='thumbsdown' name='thumbs down outline' onClick={this.handleDislike} />
+
       </React.Fragment>
     );
   }
