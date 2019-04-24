18ee3e614c85b04abfe03d3cdaba46d798fc1d69
nataliejuner <nataliereinicke@gmail.com>
2019-04-24 14:05:25 -0700

    sticky footer

--- app/javascript/components/UnauthenticatedApp.js
+++ app/javascript/components/UnauthenticatedApp.js
@@ -1,11 +1,17 @@
 import React from "react"
 import PropTypes from "prop-types"
+import Header from "./Header"
+import Footer from "./Footer"
 
 class UnauthenticatedApp extends React.Component {
   render () {
     return (
       <React.Fragment>
-        <h1>UnauthenticatedApp</h1>
+        <Header />
+        <main className="site">
+          <h1>UnauthenticatedApp</h1>
+        </main>
+        <Footer />
       </React.Fragment>
     );
   }
